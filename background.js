d66983606da798863344f42d166fbecd3708f34a
Michael <89663481+MichaelSousa00@users.noreply.github.com>
2022-02-28 18:15:27 -0500

    starter code to create google extension
    used this google tutorial to create an extension on google: https://developer.chrome.com/docs/extensions/mv3/getstarted/

---
+++ background.js
@@ -0,0 +1,8 @@
+// background.js
+
+let color = '#000000';
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ color });
+  console.log('Default background color set to %black', `color: ${color}`);
+});
